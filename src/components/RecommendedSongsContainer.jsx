af5a29edf6a70ff41409debdb833dcca19601594
cindynam <cindynam7@gmail.com>
2023-05-14 13:59:01 -0400

    Fixed formatting, organizing buttons.

--- src/components/RecommendedSongsContainer.jsx
+++ src/components/RecommendedSongsContainer.jsx
@@ -1,11 +1,21 @@
-export default function RecommendedSongsContainer({ recommendedSongs, showPlaylist, createRecommendedPlaylist, playlistLink, formatDuration}) {
+export default function RecommendedSongsContainer({
+    recommendedSongs,
+    showPlaylist,
+    createRecommendedPlaylist,
+    playlistLink,
+    formatDuration
+}) {
+
     return (
         <div className="recommended-songs">
-            <h2> Recommended for you!</h2>
-            <h3> (pssst! you can click the button again to get new recommendations!)</h3>
+            <div class="recommended-header">
+                <p> Recommended for you!</p>
+                <p class="subtext"> (pssst! you can click the button again to get new recommendations!)</p>
+            </div>
             <div className="grid">
+                {/* Displaying 9 of the recommended songs. */}
                 {recommendedSongs?.slice(0, 9).map(song => (
-                    <div className="container">
+                    <div className="container" key={song.link}>
                         <img className="image" src={song.image} alt={song.name} />
                         <div className="overlay">
                             <div className="details">
@@ -13,23 +23,30 @@ export default function RecommendedSongsContainer({ recommendedSongs, showPlayli
                                 <p>{song.artists}</p>
                                 <p>Duration: {formatDuration(song.duration)}</p>
                                 <p>Popularity: {song.popularity}</p>
-                                {song.previewUrl === null ? <p>preview unavailable</p> :
+                                {/* If the song has no preview, display a message. Otherwise, display the audio player. */}
+                                {song.previewUrl === null ? (
+                                    <p>preview unavailable</p>
+                                ) : (
                                     <p>
                                         <audio key={song.previewUrl} controls>
                                             <source src={song.previewUrl} type='audio/mp3' />
                                         </audio>
-                                    </p>}
+                                    </p>
+                                )}
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
             <div className="playlist" >
+                {/* If the playlist hasn't been created yet, display the button. If it's loading, display a loading icon. If it's ready, display the playlist. */}
                 {showPlaylist === false ? (
                     <button className="playlist-button" onClick={() => createRecommendedPlaylist()}>
                         Make it into a playlist!
                     </button>
-                ) : showPlaylist === 'loading' ? (<div className="loading" />) : (
+                ) : showPlaylist === 'loading' ? (
+                    <div className="loading" />
+                ) : (
                     <iframe
                         className='embedPlayer'
                         src={playlistLink}
@@ -43,5 +60,5 @@ export default function RecommendedSongsContainer({ recommendedSongs, showPlayli
                 )}
             </div>
         </div>
-    )
+    );
 }
